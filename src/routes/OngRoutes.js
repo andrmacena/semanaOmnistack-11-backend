8c67321e9b02aa69e34054d5cb7edecf57f161cb
André Macena <andrmacena_@outlook.com>
2020-03-25 23:31:45 -0300

    Criado arquivo de rotas para a Ong

---
+++ src/routes/OngRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const routes = express.Router()
+
+const controller = require('../controllers/OngController')
+
+routes.get('/ongs', controller.getOng)
+
+routes.post('/ongs', controller.createOng )
+
+module.exports = routes
