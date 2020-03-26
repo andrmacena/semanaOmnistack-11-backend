161adb2685873611202d9cd77ccda93c55cca3f7
André Macena <andrmacena_@outlook.com>
2020-03-25 23:31:59 -0300

    Criado controller para Ong

---
+++ src/controllers/OngController.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto')
+
+const Ong = require('../models/Ong')
+
+module.exports = {
+
+   async getOng(req, res, next) {
+      const ongs = await Ong.findAll({ attributes: ['id', 'name', 'email', 'whatsapp', 'city', 'uf'] })
+
+      return res.json(ongs)
+
+   },
+   async createOng(req, res, next) {
+      const { name, email, whatsapp, city, uf } = req.body
+
+      const id = crypto.randomBytes(4).toString('HEX')
+
+      await Ong.create({ id, name, email, whatsapp, city, uf })
+
+      return res.json('Cadastro efetuado! Seu id é: ' + id)
+
+   },
+}
