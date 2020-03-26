056eac15ca80ca3b21173f5cc8f6afa5d1b45b7e
André Macena <andrmacena_@outlook.com>
2020-03-25 23:31:33 -0300

    Criado model Incident

---
+++ src/models/Incident.js
@@ -0,0 +1,17 @@
+const { Model, DataTypes } = require('sequelize')
+
+class Incident extends Model {
+   static init(connection) {
+      super.init({
+         title: DataTypes.STRING,
+         description: DataTypes.STRING,
+         description: DataTypes.FLOAT,
+      }, { sequelize: connection })
+   }
+       //criando relacionamento entre os modelos
+       static associate(models) {
+         this.belongsTo(models.Ong, { foreignKey: 'ong_id', as: 'owner' })
+      }
+}
+
+module.exports = Incident
