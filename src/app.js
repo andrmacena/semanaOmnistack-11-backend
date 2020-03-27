1e60dafe821d27ba4b17d16e6d368ee8d6a9aabd
André Macena <andrmacena_@outlook.com>
2020-03-26 22:59:56 -0300

    Alterado app.js, adicionado rotas e tambem o modulo cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const cors = require('cors')
+
+
 const OngRoutes = require('./routes/OngRoutes')
+const IncidentRoutes = require('./routes/IncidentRoutes')
+const ProfileRoutes = require('./routes/ProfileRoutes')
+const SessionRoutes = require('./routes/SessionRoutes')
 
 const app = express()
 
+app.use(cors({
+   //origin: 'meufrontend.com.br'
+}))
 app.use(express.json())
 
 app.use(OngRoutes)
+app.use(IncidentRoutes)
+app.use(ProfileRoutes)
+app.use(SessionRoutes)
 
 require('./database')
 
