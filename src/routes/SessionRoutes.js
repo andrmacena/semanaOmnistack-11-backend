819cb5ac187ef1223d59eedc5a155032f8edd53a
André Macena <andrmacena_@outlook.com>
2020-03-30 22:44:02 -0300

    Alterado SessionRoute.js, adicionado middleware para validacao de dados

--- src/routes/SessionRoutes.js
+++ src/routes/SessionRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
 
 const routes = express.Router()
 
 const controller = require('../controllers/SessionController')
 
-routes.post('/session', controller.createSession)
+routes.post('/session', celebrate({
+   [Segments.BODY]: Joi.object().keys({
+      id: Joi.string().required()
+   })
+}), controller.createSession)
 
 module.exports = routes
