9dd89a076063d1d16d0feacd2a08269b3e0e73d7
André Macena <andrmacena_@outlook.com>
2020-03-30 22:43:51 -0300

    Alterado ProfileRoute.js, adicionado middleware para validacao de dados

--- src/routes/ProfileRoutes.js
+++ src/routes/ProfileRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
 
 const routes = express.Router()
 
 const controller = require('../controllers/ProfileController')
 
-routes.get('/ong/incidents', controller.getSpecificIncidentOng)
+routes.get('/ong/incidents', celebrate({
+   [Segments.HEADERS]: Joi.object().keys({
+      authorization: Joi.string().required()
+   })
+}), controller.getSpecificIncidentOng)
 
 module.exports = routes
