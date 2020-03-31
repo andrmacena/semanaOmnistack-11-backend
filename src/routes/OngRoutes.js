bf42e262fbb9c7fdba0de8fe2c9085e9fe1eb82b
André Macena <andrmacena_@outlook.com>
2020-03-30 22:43:37 -0300

    Alterado OngRoute.js, adicionado middleware para validacao de dados

--- src/routes/OngRoutes.js
+++ src/routes/OngRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
 
 const routes = express.Router()
 
@@ -6,6 +7,14 @@ const controller = require('../controllers/OngController')
 
 routes.get('/ongs', controller.getOng)
 
-routes.post('/ongs', controller.createOng)
+routes.post('/ongs', celebrate({
+   [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.string().required().min(10).max(11),
+      city: Joi.string().required(),
+      uf: Joi.string().required().length(2)
+   })
+}), controller.createOng)
 
 module.exports = routes
