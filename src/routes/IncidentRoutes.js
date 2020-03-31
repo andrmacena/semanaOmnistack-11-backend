e62b2c8bc0361339686efc8ddf3843fb50dd449a
André Macena <andrmacena_@outlook.com>
2020-03-30 22:43:28 -0300

    Alterado IncidentRoute.js, adicionado middleware para validacao de dados

--- src/routes/IncidentRoutes.js
+++ src/routes/IncidentRoutes.js
@@ -1,11 +1,31 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
 
 const routes = express.Router()
 
 const controller = require('../controllers/IncidentController')
 
-routes.get('/incidents', controller.getIncidents)
-routes.post('/incidents', controller.createIncident)
-routes.delete('/incidents/:id', controller.deleteIncidents)
+routes.get('/incidents', celebrate({
+   [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number()
+   })
+}), controller.getIncidents)
+
+routes.post('/incidents', celebrate({
+   [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+   }).unknown(),
+   [Segments.BODY]: Joi.object().keys({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      value: Joi.number().required()
+   })
+}),controller.createIncident)
+
+routes.delete('/incidents/:id', celebrate({
+   [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required()
+   })
+}), controller.deleteIncidents)
 
 module.exports = routes
