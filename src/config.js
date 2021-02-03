cc1fbbf8796962c9b801ad22f619ae6fa230f4d4
André Macena <andrmacena_@outlook.com>
2021-02-03 20:56:14 -0300

    Criado arquivo de configuracao para o servico de email

---
+++ src/config.js
@@ -0,0 +1,5 @@
+global.EMAIL_TMPL = 'Olá, seja bem vindo(a) ao Be the hero, esse é seu id de acesso <strong>{0}</strong>. Para acessar vá para <a href="https://bethehero-andrmacena.netlify.app/">Be The Hero</a>'
+
+module.exports = {
+    sendgridKey: process.env.SENDGRID_KEY
+}
