8691334ce1d3c129751063535643d0e231ee5441
André Macena <andrmacena_@outlook.com>
2021-02-08 23:03:59 -0300

    Alterado OngRoute, adicionando novas rotas para adicionar e recuperar imagem

--- src/routes/OngRoutes.js
+++ src/routes/OngRoutes.js
@@ -4,6 +4,7 @@ const { celebrate, Segments, Joi } = require('celebrate')
 const routes = express.Router()
 
 const controller = require('../controllers/OngController')
+const controllerImage = require('../controllers/ImageController')
 
 routes.get('/ongs', controller.getOng)
 
@@ -13,8 +14,22 @@ routes.post('/ongs', celebrate({
       email: Joi.string().required().email(),
       whatsapp: Joi.string().required().min(10).max(11),
       city: Joi.string().required(),
-      uf: Joi.string().required().length(2)
+      uf: Joi.string().required().length(2),
+      about_me: Joi.string()
    })
 }), controller.createOng)
+
+routes.post('/ongs/addImage', celebrate({
+   [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+   }).unknown(),
+   [Segments.BODY]: Joi.object().keys({
+      url_image: Joi.string().required(),
+      description: Joi.string().required()
+   })
+}),
+
+   controllerImage.createImage)
+routes.get('/ongs/images', controllerImage.getImages)
 
 module.exports = routes
