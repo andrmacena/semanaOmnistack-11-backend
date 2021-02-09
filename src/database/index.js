734baf891073efa9679f2c0e23a795d2d80aa051
André Macena <andrmacena_@outlook.com>
2021-02-08 23:04:32 -0300

    Alterado index.js do banco de dados, iniciando o relacionamento da model Image com Ong

--- src/database/index.js
+++ src/database/index.js
@@ -3,12 +3,15 @@ const dbConfig = require('../config/database')
 
 const Ong = require('../models/Ong')
 const Incident = require('../models/Incident')
+const Image = require('../models/Image')
 
 const connection = new Sequelize(dbConfig)
 
 Ong.init(connection)
 Incident.init(connection)
+Image.init(connection)
 
 Incident.associate(connection.models)
+Image.associate(connection.models)
 
 module.exports = connection
