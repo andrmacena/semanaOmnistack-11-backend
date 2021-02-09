7501a05e511ebb8fb558992ffc7c9a26e3493612
André Macena <andrmacena_@outlook.com>
2021-02-08 23:01:09 -0300

    Alterado config, adicionando chaves de acesso para envio de email e azure-storage

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 global.EMAIL_TMPL = 'Olá, seja bem vindo(a) ao Be the hero, esse é seu id de acesso <strong>{0}</strong>. Para acessar vá para <a href="https://bethehero-andrmacena.netlify.app/">Be The Hero</a>'
 
 module.exports = {
-    sendgridKey: process.env.SENDGRID_KEY
+    sendgridKey: process.env.SENDGRID_KEY,
+    containerConnectionString: process.env.AZURE_CONTAINER
 }
